3eedb48835f9d81ce11c874b1aa7bdfc05e707be
parkminjo <mj.park@sigcrew.com>
2026-01-06 12:02:45 +0900

    style: 수평으로 스크롤을 넘겨서 온도를 볼 수 있도록 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text } from 'react-native';
-
 import { View } from '@/components/Themed';
+import { Dimensions, ScrollView, StyleSheet, Text } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function TabOneScreen() {
   return (
@@ -8,12 +9,28 @@ export default function TabOneScreen() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -21,7 +38,6 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'tomato',
   },
   city: {
     flex: 1,
@@ -32,11 +48,8 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: '500',
   },
-  weather: {
-    flex: 3,
-  },
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: 'center',
   },
   temp: { fontSize: 178, marginTop: 40 },
