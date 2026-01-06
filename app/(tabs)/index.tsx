9e92537e19230cb8837de39269f5305a557f512c
parkminjo <mj.park@sigcrew.com>
2026-01-06 15:03:34 +0900

    style: 컴포넌트 배치 변경

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,20 @@
+import Fontisto from '@expo/vector-icons/Ionicons';
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
 import { ActivityIndicator, Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const API_KEY = '887180522e7f510be28eaa76ab28fa76';
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
+const icons = {
+  Clouds: 'cloudy',
+  Clear: 'sunny',
+  Rain: 'rainy',
+  Atmosphere: 'cloudy-gusts',
+  Snow: 'snow',
+  Drizzle: 'rains',
+  Thunderstorm: 'lightning',
+};
 
 export default function App() {
   const [city, setCity] = useState<string | null>('Loading...');
@@ -50,8 +61,22 @@ export default function App() {
           days.map((day, idx) => (
             <View key={idx} style={styles.day}>
               <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
-              <Text style={styles.description}>{day.weather[0].main}</Text>
-              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  width: '100%',
+                  paddingHorizontal: 80,
+                }}
+              >
+                <View>
+                  <Text style={styles.description}>{day.weather[0].main}</Text>
+                  <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+                </View>
+                <Fontisto name={icons[day.weather[0].main]} size={60} color="gray" />
+              </View>
             </View>
           ))
         )}
@@ -77,7 +102,7 @@ const styles = StyleSheet.create({
     width: SCREEN_WIDTH,
     alignItems: 'center',
   },
-  temp: { fontSize: 150 },
-  description: { fontSize: 40, marginTop: -20 },
+  temp: { fontSize: 150, fontWeight: '600' },
+  description: { fontSize: 30, marginTop: -10, color: 'gray' },
   tinyText: { fontSize: 15, color: 'gray' },
 });
